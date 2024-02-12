9e43b0b6aa6b3628971e0306b7717bde405d9d3a
Prakhar <prakhar2788@gmail.com>
2024-02-13 01:56:14 +0530

    [Prakhar] Add: A Practice code on Testing Nunit

---
+++ MyTestCase.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUINT
+{
+    [TestFixture]
+    internal class MyTestCase
+    {
+        [TestCase]
+        public void Add()
+        {
+            NunitDemo demo= new NunitDemo();
+            Assert.That(31, Is.EqualTo (demo.Add(20, 11)));
+        }
+
+        [TestCase]
+        public void Sub()
+        {
+            NunitDemo demo = new NunitDemo();
+            Assert.That(10, Is.EqualTo(demo.Sub(20, 10)));
+        }
+    }
+}
---
+++ NunitDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUINT
+{
+    internal class NunitDemo
+    {
+        public int Add(int a,int b)
+        {
+            return a + b*2;
+        }
+
+        public int Sub(int a,int b) 
+        {
+            return a - b;
+        }
+    }
+}
